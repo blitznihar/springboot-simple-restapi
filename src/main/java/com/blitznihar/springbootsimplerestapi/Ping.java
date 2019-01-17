ba6d9f9fbd2f66fabd97d72fc8c93cb9f7bdd858
blitznihar <blitznihar@gmail.com>
2019-01-17 03:11:28 -0600

    added mongodb connection for adding some delay

--- src/main/java/com/blitznihar/springbootsimplerestapi/Ping.java
+++ src/main/java/com/blitznihar/springbootsimplerestapi/Ping.java
@@ -1,5 +1,6 @@
 package com.blitznihar.springbootsimplerestapi;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,13 +10,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Ping {
 
+	@Autowired
+	public PingDataRepository repository;
+
 	@RequestMapping("/ping")
 	public ResponseEntity<?> ping(@RequestParam(value = "bounce", defaultValue = "") String bounce) {
-
+		String timestamp = java.time.LocalDateTime.now().toString();
+		repository.save(new PingData("Simple-RestAPI", bounce, timestamp));
 		if (bounce.contains("error")) {
 			return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
 		} else {
-			return new ResponseEntity<>(bounce + "@" + java.time.LocalDateTime.now().toString(), HttpStatus.OK);
+			return new ResponseEntity<>(bounce + "@" + timestamp, HttpStatus.OK);
 
 		}
 	}
---
+++ src/main/java/com/blitznihar/springbootsimplerestapi/SpringbootSimpleRestapiApplication.java
@@ -0,0 +1,14 @@
+package com.blitznihar.springbootsimplerestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSimpleRestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSimpleRestapiApplication.class, args);
+	}
+
+}
+
